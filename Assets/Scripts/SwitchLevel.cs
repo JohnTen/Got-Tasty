3c6927d7c400e5ab8c09ee5c7912a01fb8aead25
Unknown <e1235813@sina.com>
2018-05-23 11:23:29 +0800

    Initialization 2

---
+++ Assets/Scripts/SwitchLevel.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SwitchLevel : MonoBehaviour
+{
+	public void LoadScene(string levelName)
+	{
+		SceneManager.LoadScene(levelName);
+	}
+
+	public void ReloadCurrentScene()
+	{
+		SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+	}
+
+	public void ExitGame()
+	{
+		Application.Quit();
+	}
+}
---
+++ Assets/Scripts/Watermelon.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Watermelon : MonoBehaviour
+{
+    [SerializeField]
+    float maturity;
+
+}
